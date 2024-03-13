5026535e8b6299d1e5f34daaa08f01d5208c469e
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-03-13 15:21:02 +0100

    Add the adapter implementation for Gradle Enterprise plugin

--- develocity-gradle-plugin-adapters/build.gradle.kts
+++ develocity-gradle-plugin-adapters/build.gradle.kts
@@ -1,9 +1,26 @@
 plugins {
-    `java-library`
+    java
+    `java-test-fixtures`
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
+sourceSets {
+    create("compatibilityApi")
+    create("enterpriseCompatibility")
 }
 
 dependencies {
-    compileOnly(gradleApi())
+    "compatibilityApiCompileOnly"(gradleApi())
+
+    "enterpriseCompatibilityCompileOnly"(gradleApi())
+    "enterpriseCompatibilityCompileOnly"("com.gradle:gradle-enterprise-gradle-plugin:3.16.2")
+    "enterpriseCompatibilityImplementation"(sourceSets["compatibilityApi"].output)
+
+    testFixturesImplementation(gradleApi())
+    testFixturesImplementation("org.mockito:mockito-core:4.11.0")
 }
 
 java {
@@ -12,6 +29,33 @@ java {
     }
 }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
+tasks.jar {
+    from(sourceSets["compatibilityApi"].output)
+    from(sourceSets["enterpriseCompatibility"].output)
+}
+
+testing {
+    suites {
+        create<JvmTestSuite>("enterpriseCompatibilityTest") {
+            useJUnitJupiter()
+
+            dependencies {
+                implementation(gradleApi())
+                implementation(sourceSets["compatibilityApi"].output)
+                implementation(sourceSets["enterpriseCompatibility"].output)
+                implementation(testFixtures(project()))
+                implementation(platform("org.junit:junit-bom:5.10.2"))
+                implementation("org.junit.jupiter:junit-jupiter")
+                implementation("org.mockito:mockito-core:4.11.0")
+                implementation("org.mockito:mockito-junit-jupiter:4.11.0")
+                implementation("com.gradle:gradle-enterprise-gradle-plugin:3.16.2") {
+                    because("Verify that reflective proxies and adapters work with actual plugin classes")
+                }
+            }
+        }
+    }
+}
+
+tasks.named("check") {
+    dependsOn(testing.suites["enterpriseCompatibilityTest"])
 }
--- gradle-plugin-adapters/src/compatibilityApi/java/com/gradle/develocity/agent/adapters/BuildScanObfuscationAdapter.java
+++ gradle-plugin-adapters/src/compatibilityApi/java/com/gradle/develocity/agent/adapters/BuildScanObfuscationAdapter.java
@@ -4,9 +4,6 @@ import java.net.InetAddress;
 import java.util.List;
 import java.util.function.Function;
 
-/**
- * Adapter for {@link com.gradle.develocity.agent.gradle.scan.BuildScanDataObfuscationConfiguration} and {@link com.gradle.ccud.adapters.enterprise.proxies.BuildScanDataObfuscationProxy}
- */
 public interface BuildScanObfuscationAdapter {
 
     void username(Function<? super String, ? extends String> obfuscator);
