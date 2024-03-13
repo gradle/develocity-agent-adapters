d89990fe68c5a8751fa0d92c4446ac555e0d9b40
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-03-13 15:30:29 +0100

    Remove tests for the GE adapter layer

--- develocity-gradle-plugin-adapters/build.gradle.kts
+++ develocity-gradle-plugin-adapters/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     java
-    `java-test-fixtures`
 }
 
 repositories {
@@ -29,9 +28,6 @@ dependencies {
     "develocityCompatibilityCompileOnly"(gradleApi())
     "develocityCompatibilityCompileOnly"("com.gradle:develocity-gradle-plugin:3.17-rc-4")
     "develocityCompatibilityImplementation"(sourceSets["compatibilityApi"].output)
-
-    testFixturesImplementation(gradleApi())
-    testFixturesImplementation("org.mockito:mockito-core:4.11.0")
 }
 
 java {
@@ -46,47 +42,3 @@ tasks.jar {
     from(sourceSets["develocityCompatibility"].output)
 }
 
-testing {
-    suites {
-        create<JvmTestSuite>("enterpriseCompatibilityTest") {
-            useJUnitJupiter()
-
-            dependencies {
-                implementation(gradleApi())
-                implementation(sourceSets["compatibilityApi"].output)
-                implementation(sourceSets["enterpriseCompatibility"].output)
-                implementation(testFixtures(project()))
-                implementation(platform("org.junit:junit-bom:5.10.2"))
-                implementation("org.junit.jupiter:junit-jupiter")
-                implementation("org.mockito:mockito-core:4.11.0")
-                implementation("org.mockito:mockito-junit-jupiter:4.11.0")
-                implementation("com.gradle:gradle-enterprise-gradle-plugin:3.16.2") {
-                    because("Verify that reflective proxies and adapters work with actual plugin classes")
-                }
-            }
-        }
-
-        create<JvmTestSuite>("develocityCompatibilityTest") {
-            useJUnitJupiter()
-
-            dependencies {
-                implementation(gradleApi())
-                implementation(sourceSets["compatibilityApi"].output)
-                implementation(sourceSets["develocityCompatibility"].output)
-                implementation(testFixtures(project()))
-                implementation(platform("org.junit:junit-bom:5.10.2"))
-                implementation("org.junit.jupiter:junit-jupiter")
-                implementation("org.mockito:mockito-core:4.11.0")
-                implementation("org.mockito:mockito-junit-jupiter:4.11.0")
-                implementation("com.gradle:develocity-gradle-plugin:3.17-rc-4") {
-                    because("Verify that reflective proxies and adapters work with actual plugin classes")
-                }
-            }
-        }
-    }
-}
-
-tasks.named("check") {
-    dependsOn(testing.suites["enterpriseCompatibilityTest"])
-    dependsOn(testing.suites["develocityCompatibilityTest"])
-}
