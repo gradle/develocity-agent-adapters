2da90e4f2bd3635e49537bfc82fbb5848666635b
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-03-13 14:49:45 +0100

    Initialize the project

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    plugins {
+        id("com.gradle.enterprise") version "3.16.2"
+        id("com.gradle.enterprise.gradle-enterprise-conventions-plugin") version "0.7.5"
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise")
+    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "develocity-api-adapters"
+include("gradle-plugin-adapters")
