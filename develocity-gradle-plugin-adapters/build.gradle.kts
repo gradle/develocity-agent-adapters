edbf2c77f0c2b610b7e463fa3b91f20cc287253f
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-03-13 15:28:24 +0100

    Add the adapter implementation for Develocity plugin

--- develocity-gradle-plugin-adapters/build.gradle.kts
+++ develocity-gradle-plugin-adapters/build.gradle.kts
@@ -5,11 +5,18 @@ plugins {
 
 repositories {
     gradlePluginPortal()
+    maven {
+        url = uri("https://repo.grdev.net/artifactory/public")
+        content {
+            includeVersion("com.gradle", "develocity-gradle-plugin", "3.17-rc-4")
+        }
+    }
 }
 
 sourceSets {
     create("compatibilityApi")
     create("enterpriseCompatibility")
+    create("develocityCompatibility")
 }
 
 dependencies {
@@ -18,6 +25,10 @@ dependencies {
     "enterpriseCompatibilityCompileOnly"(gradleApi())
     "enterpriseCompatibilityCompileOnly"("com.gradle:gradle-enterprise-gradle-plugin:3.16.2")
     "enterpriseCompatibilityImplementation"(sourceSets["compatibilityApi"].output)
+
+    "develocityCompatibilityCompileOnly"(gradleApi())
+    "develocityCompatibilityCompileOnly"("com.gradle:develocity-gradle-plugin:3.17-rc-4")
+    "develocityCompatibilityImplementation"(sourceSets["compatibilityApi"].output)
 
     testFixturesImplementation(gradleApi())
     testFixturesImplementation("org.mockito:mockito-core:4.11.0")
@@ -32,6 +43,7 @@ java {
 tasks.jar {
     from(sourceSets["compatibilityApi"].output)
     from(sourceSets["enterpriseCompatibility"].output)
+    from(sourceSets["develocityCompatibility"].output)
 }
 
 testing {
@@ -53,9 +65,28 @@ testing {
                 }
             }
         }
+
+        create<JvmTestSuite>("develocityCompatibilityTest") {
+            useJUnitJupiter()
+
+            dependencies {
+                implementation(gradleApi())
+                implementation(sourceSets["compatibilityApi"].output)
+                implementation(sourceSets["develocityCompatibility"].output)
+                implementation(testFixtures(project()))
+                implementation(platform("org.junit:junit-bom:5.10.2"))
+                implementation("org.junit.jupiter:junit-jupiter")
+                implementation("org.mockito:mockito-core:4.11.0")
+                implementation("org.mockito:mockito-junit-jupiter:4.11.0")
+                implementation("com.gradle:develocity-gradle-plugin:3.17-rc-4") {
+                    because("Verify that reflective proxies and adapters work with actual plugin classes")
+                }
+            }
+        }
     }
 }
 
 tasks.named("check") {
     dependsOn(testing.suites["enterpriseCompatibilityTest"])
+    dependsOn(testing.suites["develocityCompatibilityTest"])
 }
