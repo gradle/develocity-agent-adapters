2f6e5e8a0f5b6d3b0cacf4b7a3fbec1e06d66e9b
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-04-04 11:49:56 +0200

    Update Develocity plugin to 3.17

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
-    id("com.gradle.enterprise") version "3.16.2"
+    id("com.gradle.develocity") version "3.17"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "1.13"
 }
 
 rootProject.name = "develocity-agent-adapters"
 include("develocity-gradle-plugin-adapters")
 include("develocity-maven-extension-adapters")
+
+develocity {
+    buildScan {
+        publishing.onlyIf { false }
+    }
+}
