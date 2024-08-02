eeb2cadcd5c08414bfe8588d22755ba61deb9e38
Gasper Kojek <gkojek@gradle.com>
2024-08-02 13:08:42 +0200

    Migrated to CCUD

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,35 @@
 plugins {
     id("com.gradle.develocity") version "3.17.6"
-    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin") version "0.7.5"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
+}
+
+val isCI = providers.environmentVariable("CI").isPresent
+
+develocity {
+    server = "https://ge.gradle.org"
+    buildScan {
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { "0.0.0.0" } }
+        }
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+
+    remote(develocity.buildCache) {
+        server = "https://eu-build-cache.gradle.org"
+        isEnabled = true
+        val accessKey = System.getenv("DEVELOCITY_ACCESS_KEY")
+        isPush = isCI && !accessKey.isNullOrEmpty()
+    }
 }
 
 rootProject.name = "develocity-agent-adapters"
+
 include("develocity-gradle-plugin-adapters")
 include("develocity-maven-extension-adapters")
